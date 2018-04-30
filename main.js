22183529233fcfeba629939d0ac38e2d7c5bd217
李貌 <710898853@qq.com>
2018-04-30 20:18:35 +0800

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+var menubar = require('menubar')
+menubar({
+dir:__dirname,
+  width: 540,
+  height: 425,
+  icon: __dirname + '/panda.png',
+  showDockIcon: false,
+  tooltip :"农历",
+  showOnRightClick: true,
+  
+})
