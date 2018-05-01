50fdf0075d61173a5262bbfeec888a6cf61dceec
李貌 <limao@startcalife.com>
2018-05-01 16:09:01 +0800

    logo

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,9 @@ menubar({
 dir:__dirname,
   width: 540,
   height: 425,
-  icon: __dirname + '/panda.png',
+  icon: __dirname + '/cat.png',
   showDockIcon: false,
   tooltip :"农历",
   // showOnRightClick: true,
-  
+
 })
